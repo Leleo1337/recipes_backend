13b2d8ec2d82556c8a2d25d8c72d9249bf87dec1
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-27 16:36:45 -0300

    feat: add base middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 import rateLimiter from './middlewares/rateLimiter';
+import errorHandlerMiddleware from './middlewares/errorHandler';
+import routeNotFound from './middlewares/routeNotFound';
 
 const app = express();
 
 app.use(helmet());
-app.use(rateLimiter)
+app.use(rateLimiter);
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('Welcome');
+});
+
+app.use(routeNotFound)
+app.use(errorHandlerMiddleware);
 
 export default app;
