2d8229578424ea44e5d0262e1d14929bae1b925b
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-01 15:48:47 -0300

    feat: create login controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,13 +1,32 @@
 import { Request, Response } from 'express';
-import User from '../models/user';
 import { StatusCodes } from 'http-status-codes';
+import User from '../models/user';
+import BadRequest from '../errors/badRequest';
 
 export async function register(req: Request, res: Response) {
 	const { name, email, password } = req.body;
-
 	const user = await User.create({ name, email, password });
-
 	const token = user.createToken();
 
 	res.status(StatusCodes.OK).json({ success: true, createdUser: { userID: user._id, username: user.name }, token });
+}
+
+export async function login(req: Request, res: Response) {
+	const { name, email, password } = req.body;
+
+	const user = await User.findOne({ $or: [{ name }, { email }] });
+	if (!user) {
+		throw new BadRequest('Incorrect user or password!');
+	}
+
+	const matchesPassword = await user.comparePassword(password);
+	if (!matchesPassword) {
+		throw new BadRequest('Incorrect user or password!');
+	}
+
+	console.log(matchesPassword);
+
+	const token = user.createToken();
+
+	res.status(StatusCodes.OK).json({ success: true, token });
 }
