ffd278352099e29ed75e879c869447d245155c0f
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-01 15:02:10 -0300

    feat: create comments endpoints

---
+++ src/routes/comments.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import validate from '../middlewares/validateJoi';
+import authMiddleware from '../middlewares/auth';
+import { commentSchema } from '../validators/commentSchema';
+import { getRecipeComments, createComment, updateComment, deleteComment } from '../controllers/comments';
+
+const router = express.Router();
+
+// - /api/v1/recipes/:recipeID/comments
+router.get('/', getRecipeComments);
+router.post('/', authMiddleware, validate(commentSchema), createComment);
+router.patch('/:commentID', authMiddleware, validate(commentSchema), updateComment);
+router.delete('/:commentID', authMiddleware, deleteComment);
+
+export default router;
