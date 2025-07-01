29af64dd45326f2826c7522f02eb1092cfa39ee6
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-01 15:54:58 -0300

    chore: rename getRecipeComments to getComments & add getComment controller

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import validate from '../middlewares/validateJoi';
 import authMiddleware from '../middlewares/auth';
 import { commentSchema } from '../validators/commentSchema';
-import { getRecipeComments, createComment, updateComment, deleteComment } from '../controllers/comments';
+import { getComments, getComment, createComment, updateComment, deleteComment } from '../controllers/comments';
 
 const router = express.Router();
 
 // - /api/v1/recipes/:recipeID/comments
-router.get('/', getRecipeComments);
+router.get('/', getComments);
+router.get('/:commentID', getComment);
 router.post('/', authMiddleware, validate(commentSchema), createComment);
 router.patch('/:commentID', authMiddleware, validate(commentSchema), updateComment);
 router.delete('/:commentID', authMiddleware, deleteComment);
