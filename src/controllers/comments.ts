a45bb1f7064f7081bc481e5b34009a8ebd9acf53
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-01 15:01:42 -0300

    TODO: create comments placeholder

---
+++ src/controllers/comments.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+export function getRecipeComments(req: Request, res: Response) {
+	res.send('dasd');
+}
+
+export function createComment(req: Request, res: Response) {
+	res.send('dasd');
+}
+export function updateComment(req: Request, res: Response) {
+	res.send('dasd');
+}
+export function deleteComment(req: Request, res: Response) {
+	res.send('dasd');
+}
