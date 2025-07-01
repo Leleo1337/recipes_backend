e8ff0ca391379e878d1d2221f52634181f6d4b84
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-01 15:53:40 -0300

    feat: enchanced loginSchema error message

--- src/validators/authSchema.ts
+++ src/validators/authSchema.ts
@@ -10,4 +10,6 @@ export const loginSchema = Joi.object({
 	name: Joi.string().min(3),
 	email: Joi.string().email(),
 	password: Joi.string().min(6).required(),
-}).or('name', 'email');
+}).or('name', 'email').messages({
+    'object.missing': 'You must provide either a username or an email along with the password.'
+});
