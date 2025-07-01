39b622434478e62af1feffccbf072f82b392a399
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-01 15:53:15 -0300

    feat: add Joi validation & login endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,11 @@
-import express from 'express'
-import { registerSchema } from '../validators/authSchema'
-import { register } from '../controllers/auth'
-import validate from '../middlewares/validateJoi'
+import express from 'express';
+import { loginSchema, registerSchema } from '../validators/authSchema';
+import { register, login } from '../controllers/auth';
+import validate from '../middlewares/validateJoi';
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/register', validate(registerSchema), register)
+router.post('/register', validate(registerSchema), register);
+router.post('/login', validate(loginSchema), login);
 
-
-export default router
+export default router;
