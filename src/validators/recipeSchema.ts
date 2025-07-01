89f848b2607431a04a8a137686bc5e2a944a8dfd
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-01 15:02:55 -0300

    chore: rename createRecipe to recipeSchema

--- src/validators/recipeSchema.ts
+++ src/validators/recipeSchema.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-export const createRecipe = Joi.object({
+export const recipeSchema = Joi.object({
 	title: Joi.string().trim().min(3).required(),
 	instructions: Joi.string().trim().min(1).required(),
 }).unknown(false);
