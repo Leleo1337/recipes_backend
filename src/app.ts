e5c7015b14b6638fa3263534f2dd508386e4777d
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-01 15:01:10 -0300

    feat: create comments endpoint

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 
 import auth from './routes/auth';
 import recipes from './routes/recipes';
+import comments from './routes/comments';
 
 import rateLimiter from './middlewares/rateLimiter';
 import errorHandlerMiddleware from './middlewares/errorHandler';
@@ -22,7 +23,8 @@ app.use(rateLimiter);
 // routes
 
 app.use('/api/v1/auth', auth);
-app.use('/api/v1/recipes', authMiddleware, recipes);
+app.use('/api/v1/recipes', recipes);
+app.use('/api/v1/recipes/:recipeID/comments', comments);
 
 app.use(routeNotFound);
 app.use(errorHandlerMiddleware);
