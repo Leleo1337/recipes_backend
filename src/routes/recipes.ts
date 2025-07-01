916532bcbf95f11b01deed9149bf58a6dc1d0801
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-01 15:03:09 -0300

    feat: create recipe endpoints

--- src/routes/recipes.ts
+++ src/routes/recipes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import validate from '../middlewares/validateJoi';
-import { getAllRecipes } from '../controllers/recipes';
+import { recipeSchema } from '../validators/recipeSchema';
+import { createRecipe, getAllRecipes, getRecipe, editRecipe, deleteRecipe } from '../controllers/recipes';
+import authMiddleware from '../middlewares/auth';
 
 const router = express.Router();
 
+// - /api/v1/recipes
 router.get('/', getAllRecipes);
+router.get('/:recipeID', getRecipe);
+router.post('/', authMiddleware, validate(recipeSchema), createRecipe);
+router.patch('/:recipeID', authMiddleware, validate(recipeSchema), editRecipe);
+router.delete('/:recipeID', authMiddleware, deleteRecipe);
 
 export default router;
