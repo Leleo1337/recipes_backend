b208584eed9e9e95166560bc428d3b436024672d
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-02 15:26:30 -0300

    feat: add image uri to recipeSchema

--- src/validators/recipeSchema.ts
+++ src/validators/recipeSchema.ts
@@ -1,6 +1,7 @@
 import Joi from 'joi';
 
 export const recipeSchema = Joi.object({
+	image: Joi.string().uri(),
 	title: Joi.string().trim().min(3).required(),
-	instructions: Joi.string().trim().min(1).required(),
+	instructions: Joi.string().trim().min(1).required()
 }).unknown(false);
