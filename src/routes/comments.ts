11cbcb4f6abdccbb408104bc587165ea2e6006df
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-02 17:37:48 -0300

    refactor: Remove unused getComment route and enable mergeParams
    added mergeParams: true to allow access to parent route parameters.

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -2,13 +2,12 @@ import express from 'express';
 import validate from '../middlewares/validateJoi';
 import authMiddleware from '../middlewares/auth';
 import { commentSchema } from '../validators/commentSchema';
-import { getComments, getComment, createComment, updateComment, deleteComment } from '../controllers/comments';
+import { getComments, createComment, updateComment, deleteComment } from '../controllers/comments';
 
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 
 // - /api/v1/recipes/:recipeID/comments
 router.get('/', getComments);
-router.get('/:commentID', getComment);
 router.post('/', authMiddleware, validate(commentSchema), createComment);
 router.patch('/:commentID', authMiddleware, validate(commentSchema), updateComment);
 router.delete('/:commentID', authMiddleware, deleteComment);
