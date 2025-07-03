ef222825ca6a49c214a435ba25eb94e5203f26e4
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-03 18:45:35 -0300

    feat: create recipe like endpoint

--- src/routes/recipes.ts
+++ src/routes/recipes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import validate from '../middlewares/validateJoi';
 import { recipeSchema } from '../validators/recipeSchema';
-import { createRecipe, getAllRecipes, getRecipe, editRecipe, deleteRecipe } from '../controllers/recipes';
+import { createRecipe, getAllRecipes, getRecipe, editRecipe, deleteRecipe, likeRecipe } from '../controllers/recipes';
 import authMiddleware from '../middlewares/auth';
 
 const router = express.Router();
@@ -15,6 +15,7 @@ router.get('/:recipeID', getRecipe);
 // private
 router.post('/', authMiddleware, validate(recipeSchema), createRecipe);
 router.patch('/:recipeID', authMiddleware, validate(recipeSchema), editRecipe);
+router.patch('/:recipeID/like', authMiddleware, likeRecipe);
 router.delete('/:recipeID', authMiddleware, deleteRecipe);
 
 export default router;
