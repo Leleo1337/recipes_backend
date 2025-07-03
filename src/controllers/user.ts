48f48fb5b71728e6a1177c572cebb72c7b9aca12
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-03 18:43:16 -0300

    feat: add getUserLikedRecipes logic

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,12 +2,13 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import Recipe from '../models/recipe';
 import Comment from '../models/comment';
+import Like from '../models/like';
 
 export async function getUserRecipes(req: Request, res: Response) {
 	const { userID } = req.user;
-	const recipe = await Recipe.find({ createdBy: userID }).sort('-date');
+	const recipes = await Recipe.find({ createdBy: userID }).sort('-createdAt');
 
-	res.status(StatusCodes.OK).json({ success: true, data: recipe });
+	res.status(StatusCodes.OK).json({ success: true, data: recipes });
 }
 
 export async function getUserComments(req: Request, res: Response) {
@@ -18,6 +19,11 @@ export async function getUserComments(req: Request, res: Response) {
 	res.status(StatusCodes.OK).json({ success: true, length: comments.length, data: comments });
 }
 
-export async function getUserLikedRecipes(req: Request, res: Response){
-    // PLACEHOLDER
+export async function getUserLikedRecipes(req: Request, res: Response) {
+	const { userID } = req.user;
+
+	const likes = await Like.find({ likedBy: userID }).populate('recipeID');
+	const likedRecipes = likes.map((like) => like.recipeID).filter((recipe) => recipe !== null);
+
+	res.status(StatusCodes.OK).json({ success: true, length: likes.length, data: likedRecipes });
 }
