b67975d3e846468c0bba439597d95a2d837e5b59
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-03 18:43:52 -0300

    chore: add uppercase on "N" in NotFound class

--- src/errors/notFound.ts
+++ src/errors/notFound.ts
@@ -1,10 +1,10 @@
 import { StatusCodes } from "http-status-codes";
 import customApiError from "./customApi";
 
-class notFound extends customApiError{
+class NotFound extends customApiError{
     constructor(message: string){
         super(message, StatusCodes.NOT_FOUND)
     }
 }
 
-export default notFound
+export default NotFound
