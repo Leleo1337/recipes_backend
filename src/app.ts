a88705865b0daa20fb48cb22b4fcd14e77bc7dc1
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-22 19:00:45 -0300

    testing vercel

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 import auth from './routes/auth';
 import recipes from './routes/recipes';
-import user from './routes/user'
+import user from './routes/user';
 import comments from './routes/comments';
 
 import rateLimiter from './middlewares/rateLimiter';
@@ -12,8 +12,15 @@ import errorHandlerMiddleware from './middlewares/errorHandler';
 import routeNotFound from './middlewares/routeNotFound';
 
 const app = express();
-app.set('trust proxy', 1);
+app.set('trust proxy', true);
 app.use(express.json());
+
+app.use((req, res, next) => {
+	console.log('IP:', req.ip, '| Headers:', req.headers['x-forwarded-for']);
+	next();
+});
+
+app.use(rateLimiter);
 
 // security
 app.use(helmet());
@@ -21,6 +28,10 @@ app.use(cors());
 app.use(rateLimiter);
 
 // routes
+
+app.get('/', (req, res) => {
+	res.send('Welcome to recipes backend');
+});
 
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/user', user);
