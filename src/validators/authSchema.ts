2b308048cd070b34c67392d2eab4bf7617a6666f
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-24 15:50:26 -0300

    refactor: add profile picture validation

--- src/validators/authSchema.ts
+++ src/validators/authSchema.ts
@@ -1,15 +1,18 @@
 import Joi from 'joi';
 
 export const registerSchema = Joi.object({
+	profilePicture: Joi.string().uri(),
 	name: Joi.string().min(3).required(),
 	email: Joi.string().email().required(),
 	password: Joi.string().min(6).required(),
 });
 
 export const loginSchema = Joi.object({
-	name: Joi.string().min(3).messages({'string.empty': '"User" is not allowed to be empty'}),
-	email: Joi.string().email().messages({'string.empty': '"User" is not allowed to be empty'}),
+	name: Joi.string().min(3).messages({ 'string.empty': '"User" is not allowed to be empty' }),
+	email: Joi.string().email().messages({ 'string.empty': '"User" is not allowed to be empty' }),
 	password: Joi.string().required(),
-}).or('name', 'email').messages({
-    'object.missing': 'You must provide either a username or an email along with the password.'
-});
+})
+	.or('name', 'email')
+	.messages({
+		'object.missing': 'You must provide either a username or an email along with the password.',
+	});
