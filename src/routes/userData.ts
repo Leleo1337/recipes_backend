da0f4745a0fb8410868cfc955091a9c1a8629ae9
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-24 15:50:09 -0300

    feat: add get user info route

--- src/routes/userData.ts
+++ src/routes/userData.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middlewares/auth';
-import { getUserComments, getUserRecipes, getUserLikedRecipes } from '../controllers/me';
+import { getUserInfo, getUserComments, getUserRecipes, getUserLikedRecipes } from '../controllers/me';
 
 const router = express.Router();
 
 // api/v1/me/
 
+router.get('/', authMiddleware, getUserInfo);
 router.get('/recipes', authMiddleware, getUserRecipes);
 router.get('/comments', authMiddleware, getUserComments);
 router.get('/liked', authMiddleware, getUserLikedRecipes);
