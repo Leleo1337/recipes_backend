4922bdfb3f18b887806f53ffb64dcc9aa0b5edf6
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-24 15:49:14 -0300

    feat: create getUserInfo

--- src/controllers/me.ts
+++ src/controllers/me.ts
@@ -3,6 +3,20 @@ import { StatusCodes } from 'http-status-codes';
 import Recipe from '../models/recipe';
 import Comment from '../models/comment';
 import Like from '../models/like';
+import User from '../models/user';
+import Unauthenticated from '../errors/unauthenticated';
+
+export async function getUserInfo(req: Request, res: Response) {
+	const { userID } = req.user;
+	const user = await User.findById(userID);
+
+	if(!user){
+		throw new Unauthenticated('User not found')
+	}
+	console.log(user)
+
+	res.status(StatusCodes.OK).json({user: {username: user.name, profilePicture: user.profilePicture}})
+}
 
 export async function getUserRecipes(req: Request, res: Response) {
 	const { userID } = req.user;
