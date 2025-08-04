2309818aec808e619d5679c6c3c9a97a2e537d19
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-04 16:38:58 -0300

    fix: add authMiddleware to /user/me
    i forgot :(

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getLoggedInUserInfo, getUserInfo, getUserCreatedRecipes, getUserLikedRecipes } from '../controllers/user';
+import authMiddleware from '../middlewares/auth';
 
 const router = express.Router();
 
 // api/v1/user/
 
-router.get('/me', getLoggedInUserInfo);
+router.get('/me', authMiddleware, getLoggedInUserInfo);
 router.get('/:userID', getUserInfo);
 router.get('/:userID/my-recipes', getUserCreatedRecipes);
 router.get('/:userID/liked-recipes', getUserLikedRecipes);
