a1dedc89be571dbec7d1d2d5446e0351673afeb2
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-11 15:48:57 -0300

    feat: create update user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -19,10 +19,23 @@ export async function getLoggedInUserInfo(req: Request, res: Response) {
 export async function getUserInfo(req: Request, res: Response) {
 	const { userID } = req.params;
 
-	const user = await User.findById(userID).select('-password -__v -email');
+	const user = await User.findById(userID).select('-password -__v');
 	if (!user) throw new NotFound('User not found');
 
 	res.status(StatusCodes.OK).json({ user });
+}
+
+export async function updateUser(req: Request, res: Response) {
+	const { userID } = req.params;
+	const { name, profilePicture, bio, email, password } = req.body;
+
+	const user = await User.findById(userID);
+
+	if (!user) throw new NotFound('User not found');
+
+	const updatedUser = await User.findByIdAndUpdate(userID, { name, profilePicture, bio, email, password }, { new: true });
+
+	res.status(StatusCodes.OK).json({ updatedUser });
 }
 
 export async function getUserCreatedRecipes(req: Request, res: Response) {
