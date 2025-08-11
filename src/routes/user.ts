e5496b73cc888d10a23f6301912bbbe7ea5ed799
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-11 15:49:36 -0300

    feat: add update user route (PATCH)

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,8 @@
 import express from 'express';
-import { getLoggedInUserInfo, getUserInfo, getUserCreatedRecipes, getUserLikedRecipes } from '../controllers/user';
+import { getLoggedInUserInfo, getUserInfo, getUserCreatedRecipes, getUserLikedRecipes, updateUser } from '../controllers/user';
 import authMiddleware from '../middlewares/auth';
+import validate from '../middlewares/validateJoi';
+import { userSchema } from '../validators/userSchema';
 
 const router = express.Router();
 
@@ -8,6 +10,7 @@ const router = express.Router();
 
 router.get('/me', authMiddleware, getLoggedInUserInfo);
 router.get('/:userID', getUserInfo);
+router.patch('/:userID', validate(userSchema), updateUser);
 router.get('/:userID/created', getUserCreatedRecipes);
 router.get('/:userID/liked', getUserLikedRecipes);
 
