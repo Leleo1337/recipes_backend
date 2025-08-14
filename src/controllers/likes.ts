8cd5b273783dfba6f24b44f1e9f0511405b4c65e
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-14 18:28:48 -0300

    feat: add checkUserLike controller

--- src/controllers/likes.ts
+++ src/controllers/likes.ts
@@ -7,7 +7,6 @@ import NotFound from '../errors/notFound';
 export async function likeRecipe(req: Request, res: Response) {
 	const { userID, username } = req.user;
 	const { recipeID } = req.params;
-    console.log(recipeID)
 	const recipe = await Recipe.findById(recipeID);
 	if (!recipe) {
 		throw new NotFound('Recipe not found');
@@ -18,17 +17,16 @@ export async function likeRecipe(req: Request, res: Response) {
 	if (!hasLiked) {
 		await Recipe.findByIdAndUpdate(recipeID, { $inc: { likesCount: 1 } });
 		await Like.create({ likedBy: userID, recipeID });
-		res.status(StatusCodes.OK).json({ success: true, msg: `User ${username} liked this post!` });
+		res.status(StatusCodes.OK).json({ success: true, state: 'liked', msg: `User ${username} liked this post!` });
 	} else {
 		await Recipe.findByIdAndUpdate(recipeID, { $inc: { likesCount: -1 } });
 		await Like.deleteOne({ likedBy: userID, recipeID });
-		res.status(StatusCodes.OK).json({ success: true, msg: `User ${username} removed his like from this post!` });
+		res.status(StatusCodes.OK).json({ success: true, state: 'removed', msg: `User ${username} removed his like from this post!` });
 	}
 }
 
 export async function getRecipeLikesCount(req: Request, res: Response) {
 	const { recipeID } = req.params;
-    console.log(req.params)
 
 	const recipe = await Recipe.findById(recipeID);
 	if (!recipe) throw new NotFound('Recipe not found!');
@@ -36,4 +34,14 @@ export async function getRecipeLikesCount(req: Request, res: Response) {
 	const likesCount = await Like.countDocuments({ recipeID: recipeID });
 
 	res.status(StatusCodes.OK).json({ likesCount });
+}
+
+export async function checkUserLike(req: Request, res: Response) {
+	const { recipeID } = req.params;
+	const { userID } = req.user;
+
+	const like = await Like.findOne({ recipeID, likedBy: userID });
+    const hasLiked = like ? true : false
+    
+	res.status(200).json({ success: true, hasLiked: hasLiked });
 }
