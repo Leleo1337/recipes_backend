a51abbb7986500c1a4387fdde9d2cc471cb678a4
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-14 18:28:58 -0300

    feat: add checkUserLike controller

--- src/routes/likes.ts
+++ src/routes/likes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import authMiddleware from '../middlewares/auth';
-import { getRecipeLikesCount, likeRecipe } from '../controllers/Likes';
+import { checkUserLike, getRecipeLikesCount, likeRecipe } from '../controllers/Likes';
 
 const router = express.Router({mergeParams: true});
 
 // - /api/v1/recipes/:recipeID/like
 
 router.get('/count', getRecipeLikesCount);
+router.get('/check', authMiddleware, checkUserLike)
 router.post('/', authMiddleware, likeRecipe);
 
 export default router;
