e0bcd29107f63121ba6d9bec01c381684f3a54e3
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-15 17:54:26 -0300

    feat: added max length 300 to comment

--- src/validators/commentSchema.ts
+++ src/validators/commentSchema.ts
@@ -3,5 +3,5 @@ import Joi from 'joi';
 export const commentSchema = Joi.object({
 	profilePicture: Joi.string().uri(),
 	username: Joi.string().trim(),
-	text: Joi.string().min(1).max(30).trim(),
+	text: Joi.string().min(1).max(300).trim(),
 });
