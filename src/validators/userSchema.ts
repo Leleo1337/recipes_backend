0bb6fd713b06e6d7319b4bd72e3f0d81f092a3c8
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-20 17:54:05 -0300

    refactor: refactoerd userSchema

--- src/validators/userSchema.ts
+++ src/validators/userSchema.ts
@@ -2,8 +2,10 @@ import Joi from 'joi';
 
 export const userSchema = Joi.object({
 	profilePicture: Joi.string(),
-	name: Joi.string(),
-	email: Joi.string(),
-	bio: Joi.string(),
-	password: Joi.string(),
+	name: Joi.string().optional(),
+	email: Joi.string().email(),
+	bio: Joi.string().optional(),
+	newPassword: Joi.string().optional(),
+	currentPassword: Joi.string().optional(),
+	socialLinks: Joi.optional(),
 });
