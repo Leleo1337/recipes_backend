0861e2d755db4657dda14ad951cee9244d22ce05
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-25 17:11:41 -0300

    feat: add social links JOI validation

--- src/validators/userSchema.ts
+++ src/validators/userSchema.ts
@@ -1,11 +1,17 @@
 import Joi from 'joi';
 
 export const userSchema = Joi.object({
-	profilePicture: Joi.string(),
+	profilePicture: Joi.string().messages({
+		'string.empty': 'O campo e-mail não pode estar vazio',
+	}),
 	name: Joi.string().optional(),
 	email: Joi.string().email(),
 	bio: Joi.string().optional(),
 	newPassword: Joi.string().optional(),
 	currentPassword: Joi.string().optional(),
-	socialLinks: Joi.optional(),
+	socialLinks: Joi.object({
+		discord: Joi.string().optional(),
+		instaram: Joi.string().uri().optional(),
+		facebook: Joi.string().uri().optional(),
+	}),
 });
